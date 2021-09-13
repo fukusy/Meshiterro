ba9932cd86e8fc7990e50e6b8dc4f46a052b51c3
fukusy <fuku1study@gmail.com>
2021-09-13 15:18:36 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '56c7100f78bade8df3c150270132c607d5dccde55402fed05e279bf474e9da11bc67a369999f2f9de06f795f19088aa092a2e5ff285099a6074ab5cabdbd1c4f'
